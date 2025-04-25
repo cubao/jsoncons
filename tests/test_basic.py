d25274d92f5b7f9e6d7f7e25ff8b4e08d13abe88
TANG ZHIXIONG <dvorak4tzx@gmail.com>
2025-04-26 00:09:45 +0800

    repl

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import json
+
 import jsoncons as m
 
 
@@ -13,3 +15,23 @@ def test_add():
 
 def test_sub():
     assert m.subtract(1, 2) == -1
+
+
+def test_repl():
+    # https://github.com/danielaparker/jsoncons/blob/master/doc/ref/jmespath/jmespath.md
+    data = {
+        "people": [
+            {"age": 20, "other": "foo", "name": "Bob"},
+            {"age": 25, "other": "bar", "name": "Fred"},
+            {"age": 30, "other": "baz", "name": "George"},
+        ]
+    }
+    repl = m.JsonQueryRepl(json.dumps(data), debug=True)
+    ret = repl.eval("people[?age > `20`].[name, age]")
+    assert ret == '[["Fred",25],["George",30]]'
+    print(json.loads(ret))
+    repl.debug = False
+    assert repl.eval("people[?age > `20`].[name, age]") == ret
+
+
+# pytest -vs tests/test_basic.py
